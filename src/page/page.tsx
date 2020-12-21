1c323a205f45e910528a4e55abaaded7db330a11
Odysa <piensengxv@gmail.com>
2020-12-21 19:31:51 +0800

    refine render function

--- src/page/page.tsx
+++ src/page/page.tsx
@@ -1,28 +1,44 @@
 import React, { FC, useState } from "react";
-import {VDom } from "../types";
-import { getComponent } from "../render/render";
+import { VDom } from "../types";
+import { render } from "../render/render";
 
 const Page: FC = () => {
-  const [state,] = useState<Array<VDom>>([{
-    type:"div",
-    style:{
-      width:100,
-      height:100,
-      backgroundColor:"red"
-    },
-    events:{
-      onClick:()=>{alert("clicked!")},
-    },
-    children:[
-      {
-        type:"span",
+  const [state] = useState<Array<VDom>>([
+    {
+      type: "div",
+      style: {
+        width: 100,
+        height: 100,
+        backgroundColor: "green",
       },
-    ],
-  }]);
+      events: {
+        onClick: () => {
+          alert("clicked!");
+        },
+      },
+      children: [
+        {
+          type: "span",
+          label: "123",
+        },
+      ],
+    },
+    {
+      type: "div",
+      style: {
+        width: 100,
+        height: 100,
+        background: "blue",
+      },
+    },
+    {
+      type:"input",
+    }
+  ]);
   return (
-    <div>
+    <div className="canvas">
       {state.map((item) => (
-        getComponent(item)
+        <div draggable>{render(item)}</div>
       ))}
     </div>
   );
--- src/render/render.ts
+++ src/render/render.ts
@@ -1,14 +1,25 @@
 import { createElement } from "react";
 import { VDom } from "../types";
 
-export function getComponent(node: VDom): any {
+export function render(node: VDom): any {
   let attributes: any = {};
   attributes.style = node.style;
   Object.assign(attributes, { ...node.events, ...node.props });
-  return createElement(node.type, attributes, generateChildren(node.children));
+  return createElement(node.type, attributes, renderChildren(node));
+}
+
+/**
+ * Decide how to render children
+ * @param node Vdom
+ */
+function renderChildren(node: VDom) {
+  if (!node.label && !node.children) return null;
+  if (!node.children) return node.label;
+  if (!node.label) return generateChildren(node.children);
+  return [node.label].concat(generateChildren(node.children));
 }
 
 function generateChildren(children: VDom[] | undefined) {
-  if (!children) return null;
-  return children.map((child) => getComponent(child));
+  if (!children) return [];
+  return children.map((child) => render(child));
 }
